adcf1c15e9d7fa0e91bc696d60dbbeb9277a3bdc
Nishant Jain <nishantkumarajain@gmail.com>
2016-11-15 18:16:30 +0530

    multiple figures and axes

--- pyplot-using-numpy.py
+++ pyplot-using-numpy.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# arange - returns evenly spaces values within the given interval
+# arange - returns evenly spaces values within the given interval (start,stop,step,dtype)
 t = np.arange(0.,5.,0.2)
 plt.plot(t,t,'r--',t,t**2,'bs',t,t**3,'g^')
 plt.show()
